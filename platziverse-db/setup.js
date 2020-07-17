02219e73ffecc8ef29f3d496bce3ed82edbabd90
Luis LC <luis.lazcanocruz@gmail.com>
2020-07-17 11:05:04 -0500

    Clase 16 -Creando una advertencia sobre el borrado de base de datos

--- platziverse-db/setup.js
+++ platziverse-db/setup.js
@@ -1,10 +1,26 @@
 'use strict'
 
 const debug = require('debug')('platziverse:db:setup')
+const inquirer = require('inquirer');
+const chalk = require('chalk');
 const config = require('./config')
 const db = require('./')
 
+const prompt = inquirer.createPromptModule()
+
 async function setup () {
+  const answer = await prompt([
+    {
+    type: 'confirm',
+    name: 'setup',
+    message: 'This will destroy your database, are you shure?',
+    }
+  ])
+
+  if(!answer.setup){
+    return console.log('Nothing happend!!')
+  }
+
   const configLocal = {
     ...config,
     logging: s => debug(s),
@@ -18,7 +34,7 @@ async function setup () {
 }
 
 function handleFatalError (err) {
-  console.error(err.message)
+  console.error(`${chalk.red('[Error: ]')} ${err.message}`)
   console.error(err.stack)
   process.exit(1)
 }
