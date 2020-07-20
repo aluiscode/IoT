164b8d1fe62785c59361422ea74fb19764ff91f2
Luis LC <luis.lazcanocruz@gmail.com>
2020-07-20 16:27:40 -0500

    Clase 30 -Cómo recibir mensajes

--- platziverse-mqtt/server.js
+++ platziverse-mqtt/server.js
@@ -1,4 +1,4 @@
-const debug = require('debug')('platzi-verse:mqtt')
+const debug = require('debug')('platziverse:mqtt')
 const mosca = require('mosca');
 const redis = require('redis');
 const chalk = require('chalk');
@@ -9,13 +9,37 @@ const backend = {
   return_buffers: true,
 }
 
-const setting = {
+const settings = {
   port: 1883,
   backend,
 }
 
-const server = new mosca.Server(setting);
+const server = new mosca.Server(settings);
 
-server.on('ready', () => {
-  console.log(`${chalk.green('[platziverse-mqtt]')} server is runnig`)
+server.on('clientConnected', client => {
+  debug(`Client Connected: ${client.id}`)
 })
+
+server.on('clientDisconnected', client => {
+  debug(`Client Disconnected: ${client.id}`)
+})
+
+server.on('published', (packet, client) => {
+  debug(`Received: ${packet.topic}`);
+  debug(`Payload: ${packet.payload}`);
+})
+
+server.on('ready', async () => {
+  console.log(`${chalk.green('[platziverse-mqtt]')} server is running`)
+})
+
+server.on('error', handleFatalError)
+
+function handleFatalError (err) {
+  console.error(`${chalk.red('[fatal error]')} ${err.message}`)
+  console.error(err.stack)
+  process.exit(1)
+}
+
+process.on('uncaughtException', handleFatalError)
+process.on('unhandledRejection', handleFatalError)
